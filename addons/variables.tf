94cd5f7d9936389044a8db3db93522c7d1ff2730
Joe Thompson <github@orion-com.com>
2025-09-29 09:13:30 -0400

    Add the metrics-server addon as part of the standard set of addons

--- addons/variables.tf
+++ addons/variables.tf
@@ -2,7 +2,7 @@ variable "basic_addons" {
   description = "A list of addons that should be installed by default.  Valid values are any EKS addon name string.  If additional resources must be installed to support a desired addon, add them to main.tf as per the example resource in that file."
   type = list(string)
   nullable = false
-  default = [ "aws-ebs-csi-driver", "snapshot-controller", "eks-pod-identity-agent" ]
+  default = [ "aws-ebs-csi-driver", "snapshot-controller", "eks-pod-identity-agent", "metrics-server" ]
 }
 
 variable "extra_addons" {
